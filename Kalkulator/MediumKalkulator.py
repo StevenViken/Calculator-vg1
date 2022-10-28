5db8a73c01294468ccdbcd92aef55dab4c4fdc89
StevenViken <stevenm@viken.no>
2022-10-28 12:45:59 +0200

    python

---
+++ Kalkulator/MediumKalkulator.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+Lånebeløp = float(input("Lånebeløp: ").replace(" ", ""))
+Løpetid = int(input("Løpetid år: ").replace(" ", ""))
+Terminer = int(input("Terminer per år: ").replace(" ", ""))
+Rente = float(input("Rente %: ").replace(" ", ""))/100
+Avdrag = Lånebeløp / (Løpetid*Terminer)
+
+numberList = []
+
+Restlån = Lånebeløp
+Renter = 0
+TerminBeløp = 0
+
+listRestlån = []
+listAvdrag = []
+listRenter = []
+listTerminBeløp = []
+
+
+for i in range(Løpetid * Terminer):
+    numberList.append(i+1)
+    if i != 0:
+        Restlån = Restlån - Avdrag
+    listRestlån.append(Restlån)
+    Renter = Restlån * Rente
+    listRenter.append(Renter)
+    listAvdrag.append(Avdrag)
+    listTerminBeløp.append(Renter+Avdrag)
+
+
+data = pd.DataFrame({"Restlån": listRestlån, "Avdrag": listAvdrag, "Renter": listRenter, "Termin Beløp": listTerminBeløp})
+print(data)
+
+width = 0.35
+
+fig, ax = plt.subplots()
+
+ax.bar(numberList, listAvdrag, width, label='Avdrag')
+ax.bar(numberList, listRenter, width, bottom=listAvdrag,
+       label='Renter')
+
+plt.ticklabel_format(style='plain')
+
+ax.set_ylabel('Kr')
+ax.set_title('Renter og avdrag')
+ax.legend()
+
+plt.show()
